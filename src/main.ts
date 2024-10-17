33a7829f803f6f760e4422e0a14ab92a7406d29c
osamaasgharsulfi <asgharusama4@gmail.com>
2024-10-17 17:28:39 +0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  
+  await app.listen(6000);
+  console.log(`Server runing on port 6000`);
+}
+bootstrap();
